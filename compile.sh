35dba9898600ceb58e1714034f0392db71feb628
Mark Burgess <markburgess1989@hotmail.com>
2018-08-17 19:47:21 +1000

    updated compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 g++ -o sim sim.c
-./sim 25 50 50
 ./sim 50 50 50
 ./sim 75 50 50
 ./sim 100 50 50
@@ -8,7 +7,6 @@ g++ -o sim sim.c
 ./sim 300 50 50
 ./sim 500 50 50
 ./sim 1000 50 50
-python gen.py data25_50_50.json
 python gen.py data50_50_50.json
 python gen.py data75_50_50.json
 python gen.py data100_50_50.json
@@ -20,3 +18,4 @@ python gen.py data1000_50_50.json
 python variance_envelope.py data50_50_50.json data75_50_50.json data100_50_50.json data150_50_50.json data200_50_50.json data300_50_50.json data500_50_50.json data1000_50_50.json
 python EBB_envelope.py formatted_data50_50_50.json 50 formatted_data75_50_50.json 75 formatted_data100_50_50.json 100 formatted_data150_50_50.json 150 formatted_data200_50_50.json 200 formatted_data300_50_50.json 300 formatted_data500_50_50.json 500 formatted_data1000_50_50.json 1000
 python graph.py
+python graph2.py formatted_data50_50_50.json 50 formatted_data75_50_50.json 75 formatted_data100_50_50.json 100 formatted_data150_50_50.json 150 formatted_data200_50_50.json 200 formatted_data300_50_50.json 300 formatted_data500_50_50.json 500 formatted_data1000_50_50.json 1000
