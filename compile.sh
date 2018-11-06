080a160d08ad0f22dea00358b84c46bdd6cd62f2
Mark Burgess <markburgess1989@hotmail.com>
2018-11-06 15:54:05 +1100

    update compile script

--- compile.sh
+++ compile.sh
@@ -15,7 +15,5 @@ python gen.py data200_50_50.json
 python gen.py data300_50_50.json
 python gen.py data500_50_50.json
 python gen.py data1000_50_50.json
-python variance_envelope.py data50_50_50.json data75_50_50.json data100_50_50.json data150_50_50.json data200_50_50.json data300_50_50.json data500_50_50.json data1000_50_50.json
-python EBB_envelope.py formatted_data50_50_50.json 50 formatted_data75_50_50.json 75 formatted_data100_50_50.json 100 formatted_data150_50_50.json 150 formatted_data200_50_50.json 200 formatted_data300_50_50.json 300 formatted_data500_50_50.json 500 formatted_data1000_50_50.json 1000
 python graph.py
 python graph2.py formatted_data50_50_50.json 50 formatted_data75_50_50.json 75 formatted_data100_50_50.json 100 formatted_data150_50_50.json 150 formatted_data200_50_50.json 200 formatted_data300_50_50.json 300 formatted_data500_50_50.json 500 formatted_data1000_50_50.json 1000
